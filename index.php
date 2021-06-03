8c302af9bef151ed5834bbcacf261a8f78bfe3dd
Jordan Radnich <jj.radnich@gmail.com>
2021-06-03 11:27:08 -0700

    adds var dump

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 if(isset($_GET['FirstName'])){//show data
+  //
+  
+  echo '<pre>';
+  var_dump($_GET);
+  echo '</pre>';
+  
   echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
 }else{//show form
   echo '
